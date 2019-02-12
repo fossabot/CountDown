ed5174c40386aec9e8fe2b463105142bc3177aea
chuncvl <cvlangelbeats@gmail.com>
2019-02-12 21:40:47 +0100

    next

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -72,8 +72,6 @@ var jsFilesToInject = [
   // All of the rest of your custom client-side js files will be injected here,
   // in no particular order.  To customize the ordering, add additional items
   // here, _above_ this one.
-  'vendor/jquery-3.3.1.min.js',
-  'vendor/bootstrap.min.js',
   'js/**/*.js'
 ];
 
